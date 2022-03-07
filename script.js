2e6662dee5b33522b1621cea5da50e1d8d618b32
Stefan3002 <92794548+Stefan3002@users.noreply.github.com>
2022-03-07 22:05:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var noImages = 3
+var inAnimationDuration = 2000
+var timeToWatch = 500
+var outAnimationDuraion = 800
+var speed = inAnimationDuration + timeToWatch + outAnimationDuraion
+
+
+var i = 1
+
+function startCarousel(){
+    if(i === 1)
+        setTimeout(function () {
+            startCarouselHelper(i)
+        }, 500)
+    else
+        setTimeout(function () {
+            startCarouselHelper(i)
+        }, speed)
+}
+
+function startCarouselHelper(){
+    if(i <= noImages) {
+        // console.log(i)
+        setImage(i)
+        i++
+        startCarousel()
+    }
+    else
+    if(i === noImages + 1){
+        i = 1
+        document.querySelector(".content img").src = ""
+        startCarousel()
+    }
+
+}
+function setImage(index){
+    const imageName = "Images/Img" + index + ".jpg"
+    const imgPLaceHolder = document.querySelector(".content img")
+    imgPLaceHolder.src = imageName
+    imgPLaceHolder.classList.replace("inactive","active")
+    setTimeout(removeAnimationClass,inAnimationDuration + timeToWatch)
+}
+function removeAnimationClass(){
+    const imgPLaceHolder = document.querySelector(".content img")
+    imgPLaceHolder.classList.replace("active","inactive")
+}
